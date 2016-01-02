494763283bb22cd3a3cd7fcf42162c42d4440c75
Cole Dunsby <coledunsby@gmail.com>
2016-01-02 18:01:01 -0500

    Initial commit

---
+++ SolarSystem/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  SolarSystem
+//
+//  Created by Cole Dunsby on 2015-12-31.
+//  Copyright © 2015 Cole Dunsby. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        return true
+    }
+
+}
+
